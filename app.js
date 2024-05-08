fda528ed1f96aea516962f789ca798eff3130da4
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-08 17:54:43 +0000

    main

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your React app's domain
-    credentials: true // Allow credentials (cookies)
+    origin: ['https://kepapro.onrender.com', 'https://kepapro-back.onrender.com'], // Replace with your React app's domain
+    credentials: true, // Allow credentials (cookies);
+    methods: ["GET", "POST", "PUT", "DELETE"],
 }));
+
 const upload = multer({ dest: 'uploads/' }); 
 
 // Middleware to check for token in incoming requests
