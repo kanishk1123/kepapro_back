cafe45b463a0b9bc5f904c9b39142f99828241ea
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-09 10:36:10 +0000

    amin

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 // CORS middleware
 app.use(
   cors({
-    origin: ["kepapro.onrender.com", "127.0.0.1:3000", "kepapro-back.onrender.com"],
+    origin: ["kepapro.onrender.com","kepapro-back.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
