e3424b088cd1cb1ff9ba1a80ca2f0b92a2ddcef1
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-11 09:11:06 +0000

    a,cs ,

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.post('/addlink',  async (req, res) => {
 
   app.get("/getall", async (req, res ,next) => {
     try {
-      const response = await video.find({season:1,ep:1});
+      const response = await video.find({season:1,ep:1,quality:480});
       res.send(response);
         next();
     } catch (error) {
