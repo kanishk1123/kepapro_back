82e923c99fa593a70668725372e818795952b41f
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-12 15:46:56 +0530

    Update video.js

--- model/video.js
+++ model/video.js
@@ -11,7 +11,7 @@ const videoSchema = mongoose.Schema({
     description:String,
     genres: [String],
     animename: String,
-    thumbnail: String,
+    thumnail: String,
     quality: Number,
     view: Number,
     popular: {
