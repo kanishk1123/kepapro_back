934e95e392f76a7704c6df0c9452ae086ca08485
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-19 02:34:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post("/register", async (req, res) => {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) // Setting expiration date to 30 days from now
 });
 
-                res.status(200).json({ message: "User created successfully" });
+                res.send(token)
             });
         });
     } catch (error) {
